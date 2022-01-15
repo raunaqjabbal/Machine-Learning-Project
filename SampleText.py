4b97fbaae3625bc509a879c1b4a0d1bbf2ed647a
raunaqjabbal <raunaq.jabbal012@gmail.com>
2022-01-15 16:25:42 +0530

    Added Classification

--- SampleText.py
+++ SampleText.py
@@ -1,22 +1,41 @@
 import pandas
 from yayo import Regression
+from yayo import Classification
 import os
-
-# Importing the dataset
 import pandas as pd
-dataset = pd.read_csv(r"C:\\Users\\Raunaq\\Desktop\\Demo\\Data.csv")
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
-y = y.reshape(len(y),1)
-
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
-demo=Regression(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test)
-print(demo.Linear())
-# print(demo.Polynomial())
-# print(demo.DecisionTree())
-# print(demo.SVM())
-# print(demo.RandomForest())
-# print(demo.XGBoost())
-# print(demo.ADABoost())
-# print(demo.CatBoost())
+
+def C():
+    dataset = pd.read_csv(r"C:\\Users\\Raunaq\\Desktop\\Machine Learning Project\\ClassificationData.csv")
+    X = dataset.iloc[:, :-1].values
+    y = dataset.iloc[:, -1].values
+    y = y.reshape(len(y),1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+    demo=Classification(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test)
+    # print(demo.Logistic())
+    # print(demo.DecisionTree())
+    # print(demo.RandomForest())
+    # print(demo.KNN())
+    # print(demo.NaiveBayes())
+    # print(demo.SVM())
+    # print(demo.XGBoost())
+    # print(demo.ADABoost())
+    # print(demo.CatBoost())
+
+def R():
+    dataset = pd.read_csv(r"C:\\Users\\Raunaq\\Desktop\\Machine Learning Project\\RegressionData.csv")
+    X = dataset.iloc[:, :-1].values
+    y = dataset.iloc[:, -1].values
+    y = y.reshape(len(y),1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+    demo=Regression(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test)
+    # print(demo.Linear())
+    # print(demo.Polynomial())
+    # print(demo.DecisionTree())
+    # print(demo.SVM())
+    # print(demo.RandomForest())
+    # print(demo.XGBoost())
+    # print(demo.ADABoost())
+    # print(demo.CatBoost())  
+    
+C()
--- yayo.py
+++ yayo.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.metrics import r2_score
+from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import cross_val_score
@@ -8,11 +9,128 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import LabelEncoder
 from xgboost import XGBRegressor
+from xgboost import XGBClassifier
 from catboost import CatBoostRegressor
+from catboost import CatBoostClassifier
 from sklearn.ensemble import AdaBoostRegressor
-from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+
+
+class Classification:
+    #print(np.concatenate((y_pred.reshape(len(y_pred),1), self.y_test.reshape(len(self.y_test),1)),1))
+    def __init__(self, X_train=None,y_train=None,X_test=None,y_test=None):
+        self.X_train=X_train
+        self.y_train=y_train
+        self.X_test=X_test
+        self.y_test=y_test
+        sc_X = StandardScaler()
+        X_train = sc_X.fit_transform(X_train) 
+        X_test = sc_X.transform(X_test)
+        self.y_train=self.y_train.reshape(-1)
+    def main(self):
+        val=[]
+        
+    def Logistic(self):
+        classifier=LogisticRegression()
+        classifier.fit(self.X_train, self.y_train)
+        y_pred=classifier.predict(self.X_test)
+        type='LogisticRegression'
+        cm = confusion_matrix(self.y_test, y_pred)
+        accuracy=accuracy_score(self.y_test, y_pred)
+        output=[classifier,type,accuracy,cm]
+        
+        return output
             
+    def DecisionTree(self):
+        classifier= DecisionTreeClassifier()
+        classifier.fit(self.X_train, self.y_train)
+        y_pred=classifier.predict(self.X_test)
+        type='DecisionTree'
+        cm = confusion_matrix(self.y_test, y_pred)
+        accuracy=accuracy_score(self.y_test, y_pred)
+        output=[classifier,type,accuracy,cm]
+        return output
+    
+    def RandomForest(self):
+        classifier=RandomForestClassifier()
+        classifier.fit(self.X_train, self.y_train)
+        y_pred=classifier.predict(self.X_test)
+        type='RandomForest'
+        cm = confusion_matrix(self.y_test, y_pred)
+        accuracy=accuracy_score(self.y_test, y_pred)
+        output=[classifier,type,accuracy,cm]
+        return output
+    
+    def KNN(self):
+        classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+        classifier.fit(self.X_train, self.y_train)
+        y_pred=classifier.predict(self.X_test)
+        type='KNearestNeighbours'
+        cm = confusion_matrix(self.y_test, y_pred)
+        accuracy=accuracy_score(self.y_test, y_pred)
+        output=[classifier,type,accuracy,cm]
+        return output
+    
+    def NaiveBayes(self):
+        classifier = GaussianNB()
+        classifier.fit(self.X_train, self.y_train)
+        y_pred=classifier.predict(self.X_test)
+        type='NaiveBayes'
+        cm = confusion_matrix(self.y_test, y_pred)
+        accuracy=accuracy_score(self.y_test, y_pred)
+        output=[classifier,type,accuracy,cm]
+        return output
+    
+    def SVM(self):
+        classifier=SVC(kernel='linear')
+        classifier.fit(self.X_train, self.y_train)
+        y_pred=classifier.predict(self.X_test)
+        type='SupportVectorMachine'
+        cm = confusion_matrix(self.y_test, y_pred)
+        accuracy=accuracy_score(self.y_test, y_pred)
+        output=[classifier,type,accuracy,cm]
+        return output
+        
+    def XGBoost(self):
+        classifier=XGBClassifier(use_label_encoder=False, eval_metric='logloss')
+        classifier.fit(self.X_train, self.y_train)
+        y_pred=classifier.predict(self.X_test)
+        type='XGBoost'
+        cm = confusion_matrix(self.y_test, y_pred)
+        accuracy=accuracy_score(self.y_test, y_pred)
+        output=[classifier,type,accuracy,cm]
+        return output
+
+    def CatBoost(self):
+        classifier=CatBoostClassifier()
+        classifier.fit(self.X_train, self.y_train)
+        y_pred=classifier.predict(self.X_test)
+        type='CatBoost'
+        cm = confusion_matrix(self.y_test, y_pred)
+        accuracy=accuracy_score(self.y_test, y_pred)
+        output=[classifier,type,accuracy,cm]
+        return output
+        
+    def ADABoost(self):
+        classifier=AdaBoostClassifier()
+        lab_enc = LabelEncoder()
+        self.y_train= lab_enc.fit_transform(self.y_train)
+        classifier.fit(self.X_train, self.y_train)
+        y_pred=classifier.predict(self.X_test)
+        type='ADABoost'
+        cm = confusion_matrix(self.y_test, y_pred)
+        accuracy=accuracy_score(self.y_test, y_pred)
+        output=[classifier,type,accuracy,cm]
+        return output
+
 
 class Regression:
     def __init__(self, X_train=None,y_train=None,X_test=None,y_test=None):
